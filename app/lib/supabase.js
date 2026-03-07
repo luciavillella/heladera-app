8b7ee391603e06821e3c4889b5c907285e55944e
luciavillella <luciavillella@gmail.com>
2026-03-07 02:25:30 -0300

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,5 +1,13 @@
+import { createBrowserClient } from '@supabase/ssr'
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 import { useAuth } from '@clerk/nextjs'
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  )
+}
 
 export function useSupabaseClient() {
   const { getToken } = useAuth()
