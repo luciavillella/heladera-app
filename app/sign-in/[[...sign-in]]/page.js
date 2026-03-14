b4137dd4d53deb78dbc7207e3bc2456dd22815f4
luciavillella <luciavillella@gmail.com>
2026-03-14 11:41:34 -0300

    Update page.js

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -2,8 +2,15 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+    <main style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+      width: '100%',
+      backgroundColor: '#F5F0E8',
+    }}>
       <SignIn />
-    </div>
+    </main>
   )
 }
